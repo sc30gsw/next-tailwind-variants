a78e159805134564e9b5c4fb30e6f23adce23569
Kaito <74711118+sc30gsw@users.noreply.github.com>
2024-01-19 16:29:29 +0900

    💄 slots機能でUI作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Button } from './components/Button'
+import { Card } from './components/Card'
 
 const Home = () => {
 	return (
 		<main className="h-screen w-screen">
 			<Button size="lg" />
+			<Card />
 		</main>
 	)
 }
